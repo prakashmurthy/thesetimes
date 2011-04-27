d1ab6980f77f7c64c45e272add13bee0b7c89572
Aaron Godin <godin.aaron@gmail.com>
2011-04-26 21:45:57 -0500

    Capified

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+require 'bundler/capistrano'
+
+default_run_options[:pty] = false
+ssh_options[:forward_agent] = true
+set :use_sudo, false
+set :user, "godinaa"
+
+set :application, "thesetimes.info"
+set :repository,  "godinaa@thesetimes.info:/home/godinaa/git/thesetimes.git"
+set :scm, :git
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :deploy_via, :remote_cache
+set :copy_compression, :bz2
+set :rails_env, 'production'
+set :deploy_to, "/home/dhusername/#{application}"
+
+role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+role :app, "your app-server here"                          # This may be the same as your `Web` server
+role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :db,  "your slave db-server here"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
